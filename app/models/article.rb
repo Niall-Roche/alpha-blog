8a3493ac14ed63111765c4b3cbb2dda566ca27f7
Niall Roche <niallroche17@gmail.com>
2016-12-23 23:38:05 +0000

    Styling and changes to homepage

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
     validates :title, presence: true, length: {minimum: 3, maximum: 10}
-    validates :description, presence: true, length: {minimum: 3, maximum: 300}
+    validates :description, presence: true, length: {minimum: 3, maximum: 3000}
 end
