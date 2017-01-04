57219dc6e4552417b2b3b0912492798abc0ba74e
Niall Roche <niallroche17@gmail.com>
2017-01-04 22:46:57 +0000

    Article/User associations added and debugger added to view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+    has_one :user
     validates :title, presence: true, length: {minimum: 3, maximum: 10}
     validates :description, presence: true, length: {minimum: 3, maximum: 3000}
+    validates :user_id, presence: true
 end
